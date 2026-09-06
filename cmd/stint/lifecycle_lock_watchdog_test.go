f1b671d2693bbb277c7a63b17c455a614ebf6c71
Marguel Gutierrez <marguel.gtz@gmail.com>
2026-09-06 01:24:12 +0100

    test: cover confirmed down preempting watchdog

--- cmd/stint/lifecycle_lock_watchdog_test.go
+++ cmd/stint/lifecycle_lock_watchdog_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"syscall"
 	"testing"
 	"time"
+
+	sessionstate "github.com/Marguelgtz/Stint/internal/session"
 )
 
 func TestDownWaitsForAnonymousLifecycleOwner(t *testing.T) {
@@ -67,4 +71,73 @@ func TestDownWaitsForVerifiedNonInterruptibleOwner(t *testing.T) {
 	}
 	releaseDown()
 	<-released
+}
+
+func TestDownYesPreemptsRecordedWatchdogHoldingLifecycleLock(t *testing.T) {
+	paths := testLifecyclePaths(t)
+	if err := paths.Ensure(); err != nil {
+		t.Fatalf("ensure paths: %v", err)
+	}
+	lockPath := filepath.Join(paths.StateDir, lifecycleLockFile)
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestLifecycleWatchdogLockHelperProcess")
+	cmd.Env = append(os.Environ(),
+		"STINT_WATCHDOG_LOCK_HELPER=1",
+		"STINT_WATCHDOG_LOCK_PATH="+lockPath,
+	)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		t.Fatalf("helper stdout pipe: %v", err)
+	}
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("start watchdog helper: %v", err)
+	}
+	defer func() {
+		if cmd.Process != nil {
+			_ = cmd.Process.Kill()
+		}
+		_ = cmd.Wait()
+	}()
+
+	ready, err := bufio.NewReader(stdout).ReadString('\n')
+	if err != nil || ready != "ready\n" {
+		t.Fatalf("watchdog helper did not acquire lock: ready=%q err=%v", ready, err)
+	}
+
+	state := sessionstate.State{
+		InstanceID:  1,
+		Status:      sessionstate.StatusRecoverable,
+		WatchdogPID: cmd.Process.Pid,
+	}
+	if err := sessionstate.Save(paths, state); err != nil {
+		t.Fatalf("save session state: %v", err)
+	}
+
+	oldArgs := os.Args
+	os.Args = []string{"stint", "down", "--yes"}
+	defer func() { os.Args = oldArgs }()
+
+	releaseDown, err := acquireLifecycleLock(paths)
+	if err != nil {
+		t.Fatalf("down --yes should preempt recorded watchdog and acquire lock: %v", err)
+	}
+	releaseDown()
+}
+
+func TestLifecycleWatchdogLockHelperProcess(t *testing.T) {
+	if os.Getenv("STINT_WATCHDOG_LOCK_HELPER") != "1" {
+		return
+	}
+	lockPath := os.Getenv("STINT_WATCHDOG_LOCK_PATH")
+	file, err := os.OpenFile(lockPath, os.O_CREATE|os.O_RDWR, 0o600)
+	if err != nil {
+		os.Exit(2)
+	}
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil {
+		os.Exit(3)
+	}
+	_, _ = os.Stdout.WriteString("ready\n")
+	for {
+		time.Sleep(time.Second)
+	}
 }
